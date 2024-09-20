534632645bab62ecb131af00cdcf3ad433b74d65
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-09-20 17:03:23 +0200

    add(cssing appreciation parrain)

--- src/Form/RapportParrainType.php
+++ src/Form/RapportParrainType.php
@@ -2,25 +2,53 @@
 
 namespace App\Form;
 
-use App\Entity\Filleul;
-use App\Entity\Parrain;
 use App\Entity\ParrainAppreciation;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RapportParrainType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('appreciation')
-            ->add('humeur')
-            ->add('travail')
-            ->add('Valider',SubmitType::class)
-
+            ->add('appreciation', null, [
+                'attr' => ['class' => 'form-control'],
+                'required' => true, // Champ obligatoire
+            ])
+            ->add('humeur', ChoiceType::class, [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ],
+                'expanded' => true, // Renders as radio buttons
+                'multiple' => false,
+                'data' => 3, // Pré-sélectionner la valeur 3
+                'required' => true, // Champ obligatoire
+                'attr' => ['class' => 'd-flex justify-content-center'], // Centre les radios
+            ])
+            ->add('travail', ChoiceType::class, [
+                'choices' => [
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    '4' => 4,
+                    '5' => 5,
+                ],
+                'expanded' => true, // Renders as radio buttons
+                'multiple' => false,
+                'data' => 3, // Pré-sélectionner la valeur 3
+                'required' => true, // Champ obligatoire
+                'attr' => ['class' => 'd-flex justify-content-center'], // Centre les radios
+            ])
+            ->add('Valider', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary'],
+            ])
         ;
     }
 
