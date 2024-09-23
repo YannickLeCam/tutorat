4b0e5232305f3cb3494192c591c6fe4372e8f714
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-09-23 15:10:07 +0200

    add(CSSing nouv direction)

--- src/Form/NouvDirectionType.php
+++ src/Form/NouvDirectionType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class NouvDirectionType extends AbstractType
@@ -15,15 +16,21 @@ class NouvDirectionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
+            ->add('nom', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => ['class' => 'form-control']
+            ])
             ->add('faculte', EntityType::class, [
                 'class' => Faculte::class,
                 'choice_label' => 'name',
+                'attr' => ['class' => 'form-select']
             ])
-            ->add('Valider', SubmitType::class);
-        ;
-    }
+            ->add('Valider', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-primary']
+            ]);
+        }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
