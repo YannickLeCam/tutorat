de0e9dc7657cc5a62b42aa0639dd4d757ead188e
YannickLeCam <149806445+YannickLeCam@users.noreply.github.com>
2024-09-23 15:11:58 +0200

    add(Nouv top)

--- src/Form/NouvTopType.php
+++ src/Form/NouvTopType.php
@@ -8,22 +8,29 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class NouvTopType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('faculte',EntityType::class,[
-                'class'=> Faculte::class,
-                'choice_label' => 'name',
-            ])
-            ->add('Valider',SubmitType::class)
-        ;
-    }
+    $builder
+    ->add('nom', TextType::class, [
+        'attr' => ['class' => 'form-control']
+    ])
+    ->add('prenom', TextType::class, [
+        'attr' => ['class' => 'form-control']
+    ])
+    ->add('faculte', EntityType::class, [
+        'class' => Faculte::class,
+        'choice_label' => 'name',
+        'attr' => ['class' => 'form-select']
+    ])
+    ->add('Valider', SubmitType::class, [
+        'attr' => ['class' => 'btn btn-primary']
+    ]);
+}
 
     public function configureOptions(OptionsResolver $resolver): void
     {
